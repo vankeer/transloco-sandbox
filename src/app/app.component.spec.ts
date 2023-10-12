6be05e801ae5b0b89658ddf5d65c875eb896fa9b
Laurens Van Keer <laurens.vankeer@colruytgroup.com>
2023-10-12 14:14:02 +0200

    fix app component basic karma/jasmine unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from "@angular/router";
+import { getTranslocoTestingModule } from "./get-transloco-testing-module";
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [AppComponent, getTranslocoTestingModule()],
+    providers: [provideRouter([])],
   }));
 
   it('should create the app', () => {
@@ -14,16 +16,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'transloco-sandbox'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('transloco-sandbox');
-  });
-
-  it('should render title', () => {
+  it('should render logo', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('transloco-sandbox app is running!');
+    const navbarBrand = compiled.querySelector('.navbar-brand')!;
+    expect(navbarBrand).toBeTruthy('Cannot find .navbar-brand element');
+
+    const img = navbarBrand.querySelector('img')!;
+    expect(img).toBeTruthy('Cannot find img element inside .navbar-brand');
+    expect(img.src).toContain('/assets/images/logo.svg', 'Logo source is incorrect');
   });
 });
